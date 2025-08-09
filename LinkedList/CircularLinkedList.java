e8ec2cfbcc3298df2a1039525875261eed85b381
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-10 06:31:36 +0800

    Create CircularLinkedList.java

---
+++ LinkedList/CircularLinkedList.java
@@ -0,0 +1,59 @@
+package linkedlist;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Christian M. Abuda
+ */
+public class LinkedList {
+    public static void main(String[] args) {
+        
+        CircularLinkedList list = new CircularLinkedList();
+    }
+}
+class Node{
+    int num = 0;
+    Node next;
+    Node prev;
+    
+    public Node(int num){
+        this.num = num;
+        this.next = null;
+        this.prev = null;
+}
+}
+class CircularLinkedList{
+    Node head;
+    Node tail;
+    
+    public void addList(int userNum){
+        Node newNode = new Node(userNum);
+        
+        if(isEmpty()){
+            head = newNode;
+            tail = newNode;
+            
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+            head.prev = tail;
+        }
+    }
+    public void deleteList(){
+        if(isEmpty()){
+            System.out.println("The list is Empty!");
+        }else{
+            head = head.next;
+            tail.next = head;
+        }
+    }
+    public boolean isEmpty(){
+        if(head == null){
+            return true;
+        }else{
+            return false;    
+        }
+    }
+}
