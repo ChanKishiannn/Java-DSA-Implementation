f4db685d873a0f82750e6c73f025794bc5d8aaf1
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-17 05:29:41 +0800

    Update LinearSearch(3).java

--- Linear Search/LinearSearch(3).java
+++ Linear Search/LinearSearch(3).java
@@ -13,28 +13,23 @@ public class LinearSearch {
        Object [] array = {2,6,4,5, "Chan"};
            
        System.out.print("Enter the Object you want to search: ");
-       Object search = input.next();
+       String search = input.next();
        
         
        Object index = linearSearch(array, search);
        
        
-       if(index != false){
+       if(index != -1){
            System.out.println("Number was on the array!");
        }else{
            System.out.println("Number was not on the array!");
        }
     }
-    private static int linearSearch(Object []array,Object value){
-        int checker = 0;
+    private static int linearSearch(Object []array, Object value){
         for(int i = 0; i < array.length; i++){
-            if(array[i] == value){
-                return checker = i;
-                
+            if(array[i].equals(value)){
+                return i; 
             }
-        }
-        if(checker != -1){
-            return bool == true;
         }
         return -1;
     }
