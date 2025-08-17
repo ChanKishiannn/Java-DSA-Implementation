d3c03f4358c14d44f750045be7d9b674210fafb8
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-18 06:22:54 +0800

    Update LinearSearch(2).java

--- Linear Search/LinearSearch(2).java
+++ Linear Search/LinearSearch(2).java
@@ -3,32 +3,36 @@ package linearsearch;
 import java.util.Scanner;
 
 /**
- *
+ * A simple demonstration of the Linear Search algorithm.
+ * 
+ * The program prompts the user to enter a number and checks whether 
+ * the number exists in a predefined integer array.
+ * 
  * @author Christian M. Abuda
  */
 public class LinearSearch {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int [] array = {2,8,9,5};
-        
+        Scanner input = new Scanner(System.in); // Scanner object for user input
+
+        // Predefined array to search from
+        int[] array = {2, 8, 9, 5};
+
+        // Prompt user for the value to search
         System.out.print("Please Enter the value you want to check: ");
         int value = input.nextInt();
-        
+
+        // Call the linearSearch method to find the index of the value
         int index = linearSearch(array, value);
-        
-        if(index != -1){
-            System.out.println("Theres a similar value found in the array!");
-        }else{
-            System.out.println("Theres no similar value in the array!");
+
+        // Display result based on whether the value was found
+        if (index != -1) {
+            System.out.println("There's a similar value found in the array!");
+        } else {
+            System.out.println("There's no similar value in the array!");
         }
     }
 
-    private static int linearSearch(int[] array, int value) {
-        for(int i=0; i < array.length; i++){
-            if(array[i] == value){
-                return i;
-            }
-        }
-        return -1;
-    }
-}
+    /**
+     * Performs a linear search on the given array.
+     *
+     * @param a*
