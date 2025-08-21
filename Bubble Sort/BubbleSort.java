7c3d1674316ce79aae98d0d89f457387d1108754
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-21 16:04:22 +0800

    Update BubbleSort.java

--- Bubble Sort/BubbleSort.java
+++ Bubble Sort/BubbleSort.java
@@ -7,17 +7,20 @@ package bubblesortimplementation;
 public class BubbleSortImplementation {
     public static void main(String[] args) {
         int [] array = {9,1,2,5,6,8,7};
-        
+        //9
         bubbleSort(array);
         
     }
     private static void bubbleSort(int [] array){
         for(int i = 0; i < array.length - 1; i++){
-            for(int j = 0; array[i] < array[j]; ++j){
-                int temp = array[j];
-                array[j] = array[i];
-                array[i] = temp;           
+            for(int j = 0; array[j] > array[i + 1]; j++){
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+                
+                
             }
+            System.out.print(array[i] + " ");
         }  
     }
 }
