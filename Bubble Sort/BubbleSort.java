0b6ba6de842301ef4f417c577e0c9d73f8196b81
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-23 07:46:18 +0800

    Update BubbleSort.java

--- Bubble Sort/BubbleSort.java
+++ Bubble Sort/BubbleSort.java
@@ -7,20 +7,47 @@ package bubblesortimplementation;
 public class BubbleSortImplementation {
     public static void main(String[] args) {
         int [] array = {9,1,2,5,6,8,7};
-        //9
+        
+        
         bubbleSort(array);
+        
+        System.out.print("This is Bubble Sort: ");
+        for(int i: array){
+            System.out.print(i + " ");
+        }
+        
+        
+        System.out.println(" ");
+        
+        reverseBubbleSort(array);
+        
+        System.out.print("This is Reverse Bubble Sort: ");
+        for(int i: array){
+            System.out.print(i + " ");
+        }
+        System.out.println("\n\n");
         
     }
     private static void bubbleSort(int [] array){
         for(int i = 0; i < array.length - 1; i++){
-            for(int j = 0; array[j] > array[i + 1]; j++){
-                int temp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = temp;
-                
-                
+            for(int j = 0; j < array.length - i - 1; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }  
             }
-            System.out.print(array[i] + " ");
+        }  
+    }
+    private static void reverseBubbleSort(int [] array){
+        for(int i = 0; i < array.length - 1; i++){
+            for(int j = 0; j < array.length - i - 1; j++){
+                if(array[j] < array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }  
+            }
         }  
     }
 }
