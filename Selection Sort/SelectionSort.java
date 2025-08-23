1f5993c3f7fe151de70767fb39d5ee85c471cdf1
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-23 08:30:12 +0800

    Update SelectionSort.java

--- Selection Sort/SelectionSort.java
+++ Selection Sort/SelectionSort.java
@@ -1,1 +1,33 @@
+package selectionsortimplementation;
 
+/**
+ *
+ * @author Christin M. Abuda
+ */
+public class SelectionSortImplementation {
+    public static void main(String[] args) {
+        int [] array = {9,1,2,5,6,8,7};
+        
+        
+        selectionSort(array);
+        
+        System.out.print("This is Selection Sort: ");
+        for(int i: array){
+            System.out.print(i + " ");
+        }
+    }
+    private static void selectionSort(int [] array){
+        for(int i = 0; i < array.length - 1; i++){
+            int min = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[min] > array[j]){
+                    min = j;
+                    
+                }  
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }  
+    }
+}
