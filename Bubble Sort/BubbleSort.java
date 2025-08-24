72db96e7b6d0a9acb6908b3fca33c479684f6f8a
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-24 12:07:01 +0800

    Update BubbleSort.java

--- Bubble Sort/BubbleSort.java
+++ Bubble Sort/BubbleSort.java
@@ -14,35 +14,12 @@ public class BubbleSortImplementation {
         System.out.print("This is Bubble Sort: ");
         for(int i: array){
             System.out.print(i + " ");
-        }
-        
-        
-        System.out.println(" ");
-        
-        reverseBubbleSort(array);
-        
-        System.out.print("This is Reverse Bubble Sort: ");
-        for(int i: array){
-            System.out.print(i + " ");
-        }
-        System.out.println("\n\n");
-        
+        }        
     }
     private static void bubbleSort(int [] array){
         for(int i = 0; i < array.length - 1; i++){
             for(int j = 0; j < array.length - i - 1; j++){
                 if(array[j] > array[j+1]){
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                }  
-            }
-        }  
-    }
-    private static void reverseBubbleSort(int [] array){
-        for(int i = 0; i < array.length - 1; i++){
-            for(int j = 0; j < array.length - i - 1; j++){
-                if(array[j] < array[j+1]){
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
