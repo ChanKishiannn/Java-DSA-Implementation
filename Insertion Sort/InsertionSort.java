0a3bc7d191be29c25df49f07b6fec50d99c5f7b7
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-25 12:42:09 +0800

    Update InsertionSort.java

--- Insertion Sort/InsertionSort.java
+++ Insertion Sort/InsertionSort.java
@@ -5,26 +5,26 @@ package insertionsortimplementation;
  */
 public class InsertionSortImplementation {
     public static void main(String[] args) {
-        int [] array = {9,1,2,5,6,8,7};
-        
+        int [] array = {8,2,9,4,3,6,1};
         
         insertionSort(array);
         
-        System.out.print("This is Insertion Sort: ");
-        for(int i: array){
-            System.out.print(i + " ");
-        }
+        System.out.println("Insertion Sort: ");
+            for(int i : array){
+                System.out.print(i);
+         }
+        System.out.println("\n");
     }
     private static void insertionSort(int [] array){
         for(int i = 1; i < array.length; i++){
             int temp = array[i];
             int j = i - 1;
             
-            while(j >= 0&& array[j] > temp){
+            while(j >= 0 && array[j] > temp){
                 array[j + 1] = array[j];
-                j--;       
+                j--;
             }
-            array[j + 1] = temp;
-        }  
+            array[j+1] = temp;
+        }
     }
 }
