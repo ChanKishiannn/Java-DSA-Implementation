e2f427b963de274d4ec2900a86ee3175bb82a5c3
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-25 10:57:17 +0800

    Update BubbleSort.java

--- Bubble Sort/BubbleSort.java
+++ Bubble Sort/BubbleSort.java
@@ -1,29 +1,29 @@
 package bubblesortimplementation;
 /**
  *
- * @author Christin M. Abuda
+ * @author Christian M. Abuda
  */
 public class BubbleSortImplementation {
-    public static void main(String[] args) {
-        int [] array = {9,1,2,5,6,8,7};
-        
-        
-        bubbleSort(array);
-        
-        System.out.print("This is Bubble Sort: ");
-        for(int i: array){
-            System.out.print(i + " ");
-        }        
+    public static void main(String[] args){
+       int [] array = {8,4,6,9,2,1};
+       
+       bubbleSort(array);
+      
+       System.out.print("Bubble Sort: ");
+       for(int i : array){
+           System.out.print(i);
+       }
+       System.out.println("\n");
     }
-    private static void bubbleSort(int [] array){
+    public static void bubbleSort(int [] array){
         for(int i = 0; i < array.length - 1; i++){
             for(int j = 0; j < array.length - i - 1; j++){
-                if(array[j] > array[j+1]){
+                if(array[j] > array[j + 1]){
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                }  
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;   
+                }
             }
-        }  
+        }
     }
 }
