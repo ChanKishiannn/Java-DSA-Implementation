0b3d129fd4e6855f74ce22de513052ecc19193aa
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-28 10:29:17 +0800

    Create QuickSortImplementaion.java

---
+++ Quick Sort/QuickSortImplementaion.java
@@ -0,0 +1,15 @@
+package quicksortimplementation;
+/**
+ *
+ * @author Christian M. Abuda
+ */
+public class QuickSortImplementation {
+    public static void main(String[] args) {
+        int [] array = {8,3,1,8,6,10};
+        
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
+    
+}
