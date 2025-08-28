3fee2a30faf0088296a84ff1470853fbbe72dc54
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-28 09:00:46 +0800

    Update MergeSort.java

--- Merge Sort/MergeSort.java
+++ Merge Sort/MergeSort.java
@@ -40,5 +40,31 @@ public class MergeSortImplementastion {
     }
     private static void merge(int [] leftArray, int [] rightArray, int [] array){
         
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+        int i = 0, l = 0, r =0;
+        
+        while(l < leftSize && r < rightSize){
+            if(leftArray[l] < rightArray[r]){
+                array[i] = leftArray[l];
+                i++;
+                l++;
+            }
+            else{
+                array[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+        while(l < leftSize){
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+        while(r < rightSize){
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
     }
 }
