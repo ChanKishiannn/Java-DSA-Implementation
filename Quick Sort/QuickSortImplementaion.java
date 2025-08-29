ac5466a4028b6b93e3bbbb4c183cdaffa89483a3
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-29 14:02:17 +0800

    Update QuickSortImplementaion.java

--- Quick Sort/QuickSortImplementaion.java
+++ Quick Sort/QuickSortImplementaion.java
@@ -8,12 +8,17 @@ public class QuickSortImplementation {
     public static void main(String[] args) {
         int [] array = {8,3,1,8,6,10};
         
+        quickSort(array, 0, array.length - 1);
+        
         for(int i : array){
             System.out.print(i + " ");
         }
     }
-    private static void quickSort(int [] array, int [] start, int [] end){
-        
+    private static void quickSort(int [] array, int start, int end){
+        if(end <= start){ 
+            return;
+        }
+            
     }
     
 }
