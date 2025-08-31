dd8cdba88d71d1239242ae44c279d0aee46a069f
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-31 11:26:55 +0800

    Update HashTable.java

--- Hash Table/HashTable.java
+++ Hash Table/HashTable.java
@@ -1,1 +1,23 @@
+package hashtableimplementation;
+import java.util.Hashtable;
 
+/**
+ *
+ * @author Christian M. Abuda
+ */
+public class HashTableImplementation {
+    public static void main(String[] args) {
+        
+        Hashtable<Integer, String> table = new Hashtable<>(10);
+        
+        table.put(100, "Eren Yieger");
+        table.put(123, "Midoriya Izuko");
+        table.put(321, "Karegaya Kazuto");
+        table.put(555, "Jack The Reiper");
+        table.put(777, "Chan Key");
+        
+        
+        System.out.println(table.get(100));
+    }
+    
+}
