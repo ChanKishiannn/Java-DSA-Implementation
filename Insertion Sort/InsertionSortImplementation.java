ec4f2efe422442da834abfab9626c9c0ed2383f7
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-31 19:46:49 +0800

    Create InsertionSortImplementation.java

---
+++ Insertion Sort/InsertionSortImplementation.java
@@ -0,0 +1,32 @@
+package insertionsortimplementation;
+/**
+ *
+ * @author Christian M. Abuda
+ */
+public class InsertionSortImplementation {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        int [] array = {8,7,1,9,5,10};
+        
+        insertionSort(array);
+        
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
+    private static void insertionSort(int [] array){
+        for(int i = 1; i < array.length; i++){
+            int temp = array[i];
+            int j = i - 1;
+            
+            while(j >= 0 && array[j] > temp){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+        }
+    }
+    
+}
+   
+    
