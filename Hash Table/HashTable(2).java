b101f863cd67b1cf05722929eec70f0001f9f8b7
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-31 11:31:37 +0800

    Create HashTable(2).java

---
+++ Hash Table/HashTable(2).java
@@ -0,0 +1,25 @@
+package hashtableimplementation;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author Christian M. Abuda
+ */
+public class HashTableImplementation {
+    public static void main(String[] args) {
+        
+        Hashtable<Integer, String> table = new Hashtable<>(10);
+        
+        table.put(100, "Eren Yieger");
+        table.put(123, "Midoriya Izuko");
+        table.put(321, "Karegaya Kazuto");
+        table.put(555, "Jack The Reiper");
+        table.put(777, "Chan Key");
+        
+        for(Integer key : table.keySet()){
+            System.out.println(key + table.get(key));
+        }
+    }
+    
+}
