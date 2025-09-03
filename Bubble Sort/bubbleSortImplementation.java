883f37ea1649f1c3288eee0bad0a675366ff3eca
Christian M. Abuda <chankishiannn@gmail.com>
2025-09-04 06:02:42 +0800

    Create bubbleSortImplementation.java

---
+++ Bubble Sort/bubbleSortImplementation.java
@@ -0,0 +1,27 @@
+package hackerRank;
+
+
+public class Solution{
+    public static void main(String[] args) {
+        int [] array = {8,7,1,9,5,10};
+        
+        bubbleSort(array);
+        
+        for(int i : array){
+        System.out.print(i + " ");
+        }
+    }
+    private static void bubbleSort(int [] array){
+        for(int i = 0; i < array.length - 1; i++){
+            for(int j = i + 1; j < array.length; j++){
+                if(array[j] < array[i]){
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+}
+   
+    
