8eb05298fc0109c99366d2724fc5d585e7788e07
Christian M. Abuda <chankishiannn@gmail.com>
2025-09-08 05:23:04 +0800

    Update SelectionSort(4).java

--- Selection Sort/SelectionSort(4).java
+++ Selection Sort/SelectionSort(4).java
@@ -1,20 +1,20 @@
-package insertionsort;
+package selectionsort;
 
 /**
  *
  * @author Christian
  */
-public class InsertionSort {
+public class SelectionSort {
     public static void main(String[] args) {
         int [] arr = {10,7,3,1,8};
         
-        insertionSort(arr);
+        selectionSort(arr);
         
         for(int i : arr){
             System.out.print(i + " ");
         }
     }
-    private static void insertionSort(int [] arr){
+    private static void selectionSort(int [] arr){
        for(int i = 0; i < arr.length - 1; i++){
            int min = i;
            
