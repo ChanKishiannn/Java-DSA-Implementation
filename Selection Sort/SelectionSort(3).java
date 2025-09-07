5de289f886d78397598450931283ff315f4400b3
Christian M. Abuda <chankishiannn@gmail.com>
2025-09-07 08:16:19 +0800

    Create SelectionSort(3).java

---
+++ Selection Sort/SelectionSort(3).java
@@ -0,0 +1,31 @@
+package insertionsort;
+
+/**
+ *
+ * @author Christian
+ */
+public class InsertionSort {
+    public static void main(String[] args) {
+        int [] arr = {10,7,3,1,8};
+        
+        insertionSort(arr);
+        
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+    private static void insertionSort(int [] arr){
+        for(int i  = 0; i < arr.length - 1; i++){
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+            
+        }
+    }
+}
