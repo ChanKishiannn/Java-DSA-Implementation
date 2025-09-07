4e111e5e22836d9700de90dfdc5f25eca9e6f19f
Christian M. Abuda <chankishiannn@gmail.com>
2025-09-07 09:29:11 +0800

    Create InsertionSort(5).java

---
+++ Insertion Sort/InsertionSort(5).java
@@ -0,0 +1,29 @@
+package insertionsort;
+
+/**
+ *
+ * @author Christian
+ */
+public class InsertionSort {
+    public static void main(String[] args) {
+        int [] arr = {10,7,3,1,8};
+        
+        insertionSort(arr);
+        
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+    private static void insertionSort(int [] arr){
+        for(int i = 1; i < arr.length; i++){
+            int temp = arr[i];
+            int j = i - 1;
+            
+            while(j >= 0 && arr[j] > temp){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
