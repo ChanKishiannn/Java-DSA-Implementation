c2ab40c5f4c3ef38214cf56628aeaa3eb2b3a1e7
Christian M. Abuda <chankishiannn@gmail.com>
2025-09-09 05:51:25 +0800

    Update MergeSortImplementation.java

--- Merge Sort/MergeSortImplementation.java
+++ Merge Sort/MergeSortImplementation.java
@@ -9,12 +9,13 @@ public class MergeSortImplementastion {
         
         mergeSort(array);
         
-        for(int i = 0; i < array.length; i++){
-            System.out.print(array[i] + " ");
+        for(int i : array){
+            System.out.print(i + " ");
         }
     }
     private static void mergeSort(int [] array){
        int length = array.length;
+       
        if(length <= 1)
        return;
        
@@ -26,14 +27,45 @@ public class MergeSortImplementastion {
        int i = 0;
        int j = 0;
        
-        for(; i < length; i++){
-            if(i < middle){
-                leftArray[i] = array[i];
-            }else{
-                rightArray[j] = array[i];
-                j++;
+            for(; i < length; i++){
+                if(i < middle){
+                    leftArray[i] = array[i];
+                }else{
+                    rightArray[j] = array[i];
+                    j++;
+                }
             }
+            mergeSort(leftArray);
+            mergeSort(rightArray);
+            merge(leftArray, rightArray, array);
         }
-       
+        private static void merge(int [] leftArray, int [] rightArray, int [] array){
+            int leftSize =  array.length / 2;
+            int rightSize = array.length - leftSize;
+            
+            int i = 0, l = 0, r = 0;
+            
+            while(l < leftSize && r < rightSize){
+                if(leftArray[l] < rightArray[r]){
+                    array[i] = leftArray[l];
+                    i++;
+                    l++;
+                }
+                else{
+                    array[i] = rightArray[r];
+                    i++;
+                    r++;
+                }
+            }
+            while(l <  leftSize){
+                array[i] = leftArray[l];
+                    i++;
+                    l++;
+            }
+            while(r < rightSize){
+                array[i] = leftArray[r];
+                    i++;
+                    r++;
+        }
     }
 }
