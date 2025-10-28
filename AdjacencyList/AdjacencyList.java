941d10be35d632c08d3c4168c54b5f68fd92ad78
Christian M. Abuda <chankishiannn@gmail.com>
2025-10-28 21:27:01 +0800

    Update AdjacencyList.java

--- AdjacencyList/AdjacencyList.java
+++ AdjacencyList/AdjacencyList.java
@@ -16,6 +16,20 @@ public class AdhacencyList {
         graph.addNode(new Node('D'));
         graph.addNode(new Node('E'));
         
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(0,3);
+        graph.addEdge(0,4);
+        
+        graph.addEdge(1,2);
+        graph.addEdge(1,3);
+        graph.addEdge(1,4);
+        
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        
+        graph.addEdge(3,4);
+        
         graph.print();
     } 
 }
@@ -29,25 +43,28 @@ class Graph{
     public void addNode(Node letter){
         nodes.add(letter);
     }
-    public void print(){
-        for(int i = 0; i < sizes; i++){
-            for(int j  = 1; j < sizes; j++){
-                for(Node node : nodes){
-                    System.out.print(node.data + " -> ");
-                }
-                if(i != 0){
-                    nodes.remove(i);
-                    sizes--;
-                }
-                System.out.println(" ");
+    public void addEdge(int scr, int dst){
+        Node s = nodes.get(scr);
+        Node d = nodes.get(dst);
+        s.neighbors.add(d);
+    }
+    
+    public void print() {
+        for (Node node : nodes) {
+            System.out.print(node.data + " -> ");
+            for (Node neighbor : node.neighbors) {
+                System.out.print(neighbor.data + " -> ");
             }
+            System.out.println(" ");
         }
     }
 }
 class Node{
     char data;
+    ArrayList<Node> neighbors;
     
     Node(char data){
         this.data = data;
+        this.neighbors = new ArrayList<Node>(); 
     }
 }
